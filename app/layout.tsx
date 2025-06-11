62c5a03ab9a1e30bd815e1ed6ba04c45bb197ad1
Sunny Ugwu <sunnyugwu2011@gmail.com>
2025-06-11 09:02:36 +0100

    adds locadapt multilingual script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer' // <-- Import Footer
 import { Italiana, Karla } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from 'next/script';
 
 const italiana = Italiana({
   subsets: ["latin"],
@@ -23,6 +24,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${italiana.variable} ${karla.variable}`}>
+      <head>
+        <link
+    rel="stylesheet"
+    href="https://cdn.locadapt.com/locadapt.min.css"
+/>
+<Script
+    src="https://cdn.locadapt.com/locadapt.min.js"
+    data-project-id="fd57891c-6b4a-4c4a-a9c9-6b4ac4919780"
+></Script>
+      </head>
       <body className="font-sans antialiased">
         <NavBar />
         <main>{children}</main>
