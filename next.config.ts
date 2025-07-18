f163bdd6eaae162f3f7de3d3510df4672b4c1589
Sunny Ugwu <sunnyugwu2011@gmail.com>
2025-07-18 12:27:01 +0100

    force HTTPS redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  output: 'export',
+ async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'stylznsmylz.com',
+          },
+        ],
+        destination: 'https://stylznsmylz.com/:1',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
